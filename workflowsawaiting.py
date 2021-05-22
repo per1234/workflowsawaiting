5b713f3ac3452d48a73584bf2dbb88ae5cb5b998
per1234 <accounts@perglass.com>
2021-05-22 02:31:05 -0700

    (f) code

--- workflowsawaiting.py
+++ workflowsawaiting.py
@@ -10,34 +10,34 @@ import ghapi.page
 import yaml
 import yaml.parser
 
+if "GITHUB_TOKEN" not in os.environ:
+    print("ERROR: required environment variable GITHUB_TOKEN not set.")
+    sys.exit(1)
+
 gh_api = ghapi.core.GhApi(token=os.environ["GITHUB_TOKEN"])
 username = gh_api.users.get_authenticated().login
 
 
 def main():
     """The primary function."""
-    if "GITHUB_TOKEN" not in os.environ:
-        print("ERROR: required environment variable GITHUB_TOKEN not set.")
-        sys.exit(1)
-
     with open(file=argument.configuration_path, mode="r", encoding="utf-8") as configuration_file:
         configuration_list = yaml.load(stream=configuration_file, Loader=yaml.SafeLoader)
 
     awaiting_count = 0
 
     repositories_report = []
-    repositories = get_repositories(configuration_list=configuration_list)
-    for repository in repositories:
-        verbose_print("Getting workflow runs for", repository["object"].full_name)
-        verbose_print("permissions:", repository["permissions"])
+    repositories_data = get_repositories_data(configuration_list=configuration_list)
+    for repository_data in repositories_data:
+        verbose_print("Getting workflow runs for", repository_data["object"].full_name)
+        verbose_print("permissions:", repository_data["permissions"])
         repository_report = {
-            "name": repository["object"].full_name,
-            "permissions": repository["permissions"],
-            "runs": get_runs(repository=repository),
+            "name": repository_data["object"].full_name,
+            "permissions": repository_data["permissions"],
+            "runs": get_runs(repository_data=repository_data),
         }
 
         if repository_report["runs"]:
-            # There were workflow runs awaiting approval.
+            # There were workflow runs awaiting approval
             awaiting_count += len(repository_report["runs"])
             repositories_report.append(repository_report)
 
@@ -52,25 +52,25 @@ def main():
             json.dump(obj=report, fp=report_file, indent=2)
 
 
-def get_repositories(configuration_list):
-    """Returns the list of repositories defined by the configuration
+def get_repositories_data(configuration_list):
+    """Return data for the repositories defined by the configuration.
 
     Keyword arguments:
     configuration_list -- the list of configuration objects specified by the user's configuration file
     """
     verbose_print("Generating list of repositories")
-    repositories = []
+    repositories_data = []
     for configuration_element in configuration_list:
         # Handle ignored repos:
         if "action" in configuration_element and configuration_element["action"] == "ignore":
-            # Remove any matching repos from the list.
-            repositories[:] = list(
+            # Remove any matching repos from the list
+            repositories_data[:] = list(
                 itertools.filterfalse(
                     predicate=(
                         lambda repository: configuration_element["name"] == repository["object"].full_name
                         or configuration_element["name"] == repository["object"].owner.login,
                     ),
-                    iterable=repositories,
+                    iterable=repositories_data,
                 )
             )
 
@@ -83,30 +83,38 @@ def get_repositories(configuration_list):
             repository_name = split_name[1]
 
         if repository_name:
-            repository = {"object": gh_api.repos.get(owner=owner_name, repo=repository_name)}
-            repository["permissions"] = get_permissions(repository_object=repository["object"])
-            if in_scope(repository=repository, configuration_element=configuration_element):
-                repositories.append(repository)
+            repository_data = {"object": gh_api.repos.get(owner=owner_name, repo=repository_name)}
+            repository_data["permissions"] = get_permissions(repository_object=repository_data["object"])
+            if in_scope(repository_data=repository_data, configuration_element=configuration_element):
+                repositories_data.append(repository_data)
         else:
             user = gh_api.users.get_by_username(username=owner_name)
             if user.type == "Organization":
-                repositories_pages = ghapi.page.paged(oper=gh_api.repos.list_for_org, org=owner_name)
+                repositories_pages = ghapi.page.pages(
+                    oper=gh_api.repos.list_for_org, n_pages=gh_api.last_page(), org=owner_name
+                )
             else:
-                repositories_pages = ghapi.page.paged(oper=gh_api.repos.list_for_user, username=owner_name)
+                repositories_pages = ghapi.page.pages(
+                    oper=gh_api.repos.list_for_user, n_pages=gh_api.last_page(), username=owner_name
+                )
             for repositories_page in repositories_pages:
                 for repository_object in repositories_page:
-                    repository = {
+                    repository_data = {
                         "object": repository_object,
                         "permissions": get_permissions(repository_object=repository_object),
                     }
-                    if in_scope(repository=repository, configuration_element=configuration_element):
-                        repositories.append(repository)
+                    if in_scope(repository_data=repository_data, configuration_element=configuration_element):
+                        repositories_data.append(repository_data)
 
-    return repositories
+    return repositories_data
 
 
 def get_permissions(repository_object):
-    """Return the user's permissions level in the repository"""
+    """Return the user's permissions level in the repository.
+
+    Keyword arguments:
+    repository_object -- a GitHub API repository object
+    """
     try:
         return gh_api.repos.get_collaborator_permission_level(
             owner=repository_object.owner.login, repo=repository_object.name, username=username
@@ -115,41 +123,42 @@ def get_permissions(repository_object):
         return None
 
 
-def in_scope(repository, configuration_element):
-    """Returns whether the given repository is in scope for the configuration
+def in_scope(repository_data, configuration_element):
+    """Return whether the given repository is in scope for the configuration.
 
     Keyword arguments:
+    repository_data -- data for the repository
     configuration_element -- the configuration object that yielded the repository
-    repository -- a repository object
     """
     if "scope" in configuration_element and configuration_element["scope"] == "all":
         return True
 
-    # Determine if user has sufficient permissions in the repository to approve the workflow run
-    return not repository["object"].archived and (
-        repository["permissions"] == "write" or repository["permissions"] == "admin"
+    # Determine if user has sufficient permissions in the repository_data to approve the workflow run
+    return not repository_data["object"].archived and (
+        repository_data["permissions"] == "write" or repository_data["permissions"] == "admin"
     )
 
 
-def get_runs(repository):
-    """Returns a list of URLs for workflow runs awaiting approval in the given repo
+def get_runs(repository_data):
+    """Return a list of URLs for workflow runs awaiting approval in the given repo.
 
     Keyword arguments:
-    repository -- a repository object
+    repository_data -- data for the repository
     """
     run_urls = []
-    runs_pages = ghapi.page.paged(
+    runs_pages = ghapi.page.pages(
         oper=gh_api.actions.list_workflow_runs_for_repo,
-        owner=repository["object"].owner.login,
-        repo=repository["object"].name,
+        n_pages=gh_api.last_page(),
+        owner=repository_data["object"].owner.login,
+        repo=repository_data["object"].name,
         event="pull_request",
         status="action_required",
     )
     for runs_page in runs_pages:
-        if not runs_page.workflow_runs:
-            # The paged generator gets stuck in an infinite loop on the total_count key after all the runs are
-            # iterated over
-            break
+        # if not runs_page.workflow_runs:
+        #     # The paged generator gets stuck in an infinite loop on the total_count key after all the runs are
+        #     # iterated over
+        #     break
         for run in runs_page.workflow_runs:
             print("Run", run.html_url, "needs approval")
             run_urls.append(run.html_url)
@@ -158,7 +167,7 @@ def get_runs(repository):
 
 
 def verbose_print(*print_arguments):
-    """Print output when in verbose mode"""
+    """Print output when in verbose mode."""
     if argument.verbose:
         print(*print_arguments)
 
